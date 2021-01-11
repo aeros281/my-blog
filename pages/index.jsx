d70dcf2460834118175730e0b03a567c79de9364
aeros281 <tranletuananh1992@gmail.com>
2021-01-11 22:46:16 +0700

    Add database connection layer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 import typo from '../styles/typography.module.scss'
 import Layout, { BLOG_NAME } from '../components/layout'
 import Counter from '../components/core/counter/counter'
+import { getAllPosts } from '../lib/blog'
 
-export default function Home({ wreckedCounter }) {
+export default function Home({ wreckedCounter, posts }) {
   return (
     <Layout>
       <Head>
@@ -18,6 +20,18 @@ export default function Home({ wreckedCounter }) {
         <p>Just FYI,</p>
         <Counter countNum={wreckedCounter} subtitle={'..people got wretch by the little old hag today'} />
       </section>
+      <section>
+        <h2 className={typo.headingLg}>Blog</h2>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <Link href={`/posts/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
 }
@@ -25,11 +39,13 @@ export default function Home({ wreckedCounter }) {
 export async function getStaticProps() {
   const currentTime = new Date()
   const secondPassed = currentTime.getSeconds() + (60 * currentTime.getMinutes()) + (3600 * currentTime.getHours())
+
+  const posts = await getAllPosts()
   return {
     props: {
       wreckedCounter: secondPassed,
-      revalidate: 1,
+      posts: posts,
     },
-
+    revalidate: 1,
   }
 }
